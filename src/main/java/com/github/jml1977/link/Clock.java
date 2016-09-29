ff313d26560a1be816eb68373007f9aff9da7e9d
James L <james.m.lowery@gmail.com>
2016-09-29 08:01:42 +0200

    clock update

--- src/main/java/com/github/jml1977/link/Clock.java
+++ src/main/java/com/github/jml1977/link/Clock.java
@@ -1,9 +1,13 @@
 package com.github.jml1977.link;
 
 public class Clock {
-	private final long startTime = System.nanoTime() / 1000;
+	private final long startTime = now();
 
 	public long micros() {
-		return (System.nanoTime() / 1000) - startTime;
+		return now() - startTime;
+	}
+
+	private static long now() {
+		return System.nanoTime() / 1000;
 	}
 }
